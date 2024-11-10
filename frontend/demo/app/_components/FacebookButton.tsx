a2de839b4c96bf7c0b4fc68215dd9ca7f2ce5d19
Pessina <fs.pessina@gmail.com>
2024-11-10 17:30:17 +0700

    Improve lib

--- frontend/demo/app/_components/FacebookButton.tsx
+++ frontend/demo/app/_components/FacebookButton.tsx
@@ -1,15 +1,33 @@
 'use client'
 
-
 import { Button } from "@/components/ui/button"
-import { useFacebookAuth } from '../hooks/useFacebookAuth';
-
+import { useFacebookAuth } from '../../hooks/useFacebookAuth';
+import { useState } from "react";
 
 export default function FacebookButton() {
-    const { status, message, initiateLogin, isLoading } = useFacebookAuth();
+    const [isLoading, setIsLoading] = useState(false);
+    const { initiateLogin } = useFacebookAuth({
+        appId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID || '',
+        onSuccess: (idToken) => {
+            console.log('Successfully logged in with Facebook', idToken);
+            setIsLoading(false);
+        },
+        onError: (error) => {
+            console.error('Error logging in with Facebook', error);
+            setIsLoading(false);
+        },
+    });
+
+    const handleLogin = async () => {
+        setIsLoading(true);
+        await initiateLogin({
+            nonce: 'test-123',
+        });
+        setIsLoading(false);
+    };
 
     return (
-        <Button onClick={initiateLogin} disabled={isLoading} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Button onClick={handleLogin} disabled={isLoading} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             {isLoading ? 'Loading...' : 'Login with Facebook'}
         </Button>
     );
