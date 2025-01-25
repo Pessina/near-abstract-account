440b633a69f73b8d00bd3715cbb135447212119f
Pessina <fs.pessina@gmail.com>
2025-01-25 21:28:29 +0700

    Fix import

--- frontend/app/transaction/page.tsx
+++ frontend/app/transaction/page.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Transaction } from "@/contracts/AbstractAccountContract/AbstractAccountContract"
+import { Transaction } from "@/contracts/AbstractAccountContract/types/transaction"
 import { useAbstractAccountContract } from "@/contracts/AbstractAccountContract/useAbstractAccountContract"
 import { AbstractAccountContractBuilder } from "@/contracts/AbstractAccountContract/utils/auth"
 
