e94d86584d2aee1dc0749ed836d77a4256330ba8
Pessina <fs.pessina@gmail.com>
2025-03-19 23:26:53 -0300

    Include OIDC verification on Solana

--- solana/contracts/programs/contracts/src/lib.rs
+++ solana/contracts/programs/contracts/src/lib.rs
@@ -1,6 +1,19 @@
 use anchor_lang::prelude::*;
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
+use rsa::{pkcs1v15::VerifyingKey, signature::Verifier, BigUint, RsaPublicKey};
+use sha2::Sha256;
 
 declare_id!("8KrLertSDNhWZ8hiYiPEeH8UnXs7Q7w9F52pwcbxXBcD");
+
+// #[derive(borsh::BorshSerialize, borsh::BorshDeserialize)]
+// pub struct PublicKey {
+//     pub kid: String,
+//     pub n: String,
+//     pub e: String,
+//     pub alg: String,
+//     pub kty: String,
+//     pub use_: String,
+// }
 
 #[program]
 pub mod contracts {
@@ -17,6 +30,41 @@ pub mod contracts {
 
     pub fn increment(ctx: Context<Increment>) -> Result<()> {
         ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
+        Ok(())
+    }
+
+    pub fn verify(_ctx: Context<Verify>, oidc_token: String, n: String, e: String) -> Result<()> {
+        let parts: Vec<&str> = oidc_token.split('.').collect();
+        if parts.len() != 3 {
+            panic!("Invalid JWT format - token must have 3 parts");
+        }
+        let (header_b64, payload_b64, sig_b64) = (parts[0], parts[1], parts[2]);
+
+        let n = URL_SAFE_NO_PAD
+            .decode(&n)
+            .expect("Failed to decode public key modulus");
+        let e = URL_SAFE_NO_PAD
+            .decode(&e)
+            .expect("Failed to decode public key exponent");
+
+        let rsa_pubkey = RsaPublicKey::new(BigUint::from_bytes_be(&n), BigUint::from_bytes_be(&e))
+            .expect("Failed to construct RSA public key");
+
+        let verifying_key = VerifyingKey::<Sha256>::new(rsa_pubkey);
+        let message = format!("{}.{}", header_b64, payload_b64);
+        let signature = URL_SAFE_NO_PAD
+            .decode(sig_b64)
+            .expect("Failed to decode JWT signature");
+
+        let result = verifying_key
+            .verify(
+                message.as_bytes(),
+                &rsa::pkcs1v15::Signature::try_from(signature.as_slice())
+                    .expect("Failed to parse signature"),
+            )
+            .is_ok();
+
+        msg!("Verification result: {}", result);
         Ok(())
     }
 }
@@ -50,3 +98,6 @@ pub struct Increment<'info> {
 pub struct Counter {
     count: u64,
 }
+
+#[derive(Accounts)]
+pub struct Verify {}
--- solana/sdk/app/page.tsx
+++ solana/sdk/app/page.tsx
@@ -100,6 +100,38 @@ export default function Home() {
     // console.log("Current count:", currentCount.count);
   };
 
+  const verify = async () => {
+    if (!wallet.publicKey || !wallet.signTransaction || !program) {
+      setMessage("Wallet not fully connected");
+      return;
+    }
+
+    const tx = await program.methods
+      .verify(
+        "eyJhbGciOiJSUzI1NiIsImtpZCI6ImVlMTkzZDQ2NDdhYjRhMzU4NWFhOWIyYjNiNDg0YTg3YWE2OGJiNDIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3Mzk5MTEwNjk3OTctaWRwMDYyODY2OTY0Z2JuZG82NjkzaDMydGdhNWN2bDEuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3Mzk5MTEwNjk3OTctaWRwMDYyODY2OTY0Z2JuZG82NjkzaDMydGdhNWN2bDEuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTc5MDI4NTUzNzMxNTc0MTAzMzAiLCJlbWFpbCI6ImZzLnBlc3NpbmFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5iZiI6MTc0MjQzNjUzOCwibmFtZSI6IkZlbGlwZSBQZXNzaW5hIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0pLSmJVeUJWcUNCdjRxVkdPRFNuVlRnTEhQS04wdFZPTUlNWFZpdWtncmQtMHRmZWRVPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkZlbGlwZSIsImZhbWlseV9uYW1lIjoiUGVzc2luYSIsImlhdCI6MTc0MjQzNjgzOCwiZXhwIjoxNzQyNDQwNDM4LCJqdGkiOiJlOWI2NTM3MGUzOTdjNjBiNmJjNWExYjYxZDkxMjM3NmRlNTU3YzQ3In0.R-suNXbwxS4vnNu4cOQ65WgSjj8Gbyf7e1sMYJJvOxCq3UdvhwJeJHS7qIdvDKLsjW_piUbj5SZndHyzpXUrbn0Dh7b4Gs00A-gNAt8QEBU6zno89O8W4Aev2mRIY75wXTDVGIxi_NVdwyXPC_AfMAnHI5ZTUmvxcKNXDHCD3LkOdnwHn_jW4Tn83LTxUwbdu2Ma7m4BiZwodb6pO5Fo5hb4dXGEtTbQBbU5HjxZSYy1myLuI8CsNlraCO4NlLJY2JtT27RsJzH9f0bh2MoSD4iDG-abEopPiP7KomH4l8enfQpKRe1hhjTolkJVihFL2k7OYVttiX-Zh0Q8Olqong",
+        "rxLSY1w1gu-IzjVkBEqZXWcA1adZ15VmGpPYKpt8N_MXbgwICCy__iPVvuvSqetTvshwxEEK8ZcbmEyG_rcPiIBBoHYdtVb_cTlNR7JfT2ZOFKZUW1y3FBnZ2TTBHCgCJ9N7d-r6doQ-NI0GXOWzZh5Q9CPc9NDZoe8RfH-RE4m1RNGAukKThomofesSyw5OY92WxK9sfwTshmlK-J-wFB2OlN7xuwF3Rns_CJLdnajhf5XVMdNqEeSk3Fyoi72qWRQbDhfEhT5qcpkMX42BgWbmlom0ZPwPPhyyd9jrfFNN0BNgvF2kPD2eJ8qsaaUAZn4DBvcTpC5RhiwSY_AB8w",
+        "AQAB"
+      )
+      .rpc();
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+    await connection.confirmTransaction(
+      {
+        signature: tx,
+        blockhash,
+        lastValidBlockHeight,
+      },
+      "confirmed"
+    );
+    console.log("Transaction confirmed:", tx);
+
+    // const currentCount = await program.account.counter.fetch(
+    //   counterKeyPair.publicKey
+    // );
+
+    // console.log("Current count:", currentCount.count);
+  };
+
   const incrementCounter = async () => {
     if (!wallet.publicKey || !wallet.signTransaction || !program) {
       setMessage("Wallet not fully connected");
@@ -139,6 +171,7 @@ export default function Home() {
             <button onClick={initializeContract}>Initialize Contract</button>
             <button onClick={initializeCounter}>Initialize Counter</button>
             <button onClick={incrementCounter}>Increment Counter</button>
+            <button onClick={verify}>Verify</button>
           </>
         )}
       </div>
